a789c53e516dd6e9cb1d79bc8ed11fe5eb193a2d
Abubakar-sidik <absiki74@gmail.com>
2026-02-08 01:20:00 -0900

    Fix logo2 file extension from PNG to png

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo2 from "./logo2.PNG";
+import logo2 from "./logo2.png";
 import search_icon from "./search_icon.svg";
 import company_icon from "./company_icon.svg";
 import microsoft_logo from "./microsoft_logo.svg";
